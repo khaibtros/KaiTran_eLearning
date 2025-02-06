52aa69b02fc35112f8b146aeabfb7142221d96f6
khaibtros <trankhai15052005@gmail.com>
2025-02-06 20:02:48 +0700

    logoutUser

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -8,6 +8,7 @@ import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
 import { sendToken } from "../utils/jwt";
+import { redis } from "../utils/redis";
 
 // register user
 interface IRegistrationBody {
@@ -161,6 +162,28 @@ export const loginUser = CatchAsyncError(
       sendToken(user, 200, res);
     } catch (error: any) {
 
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+interface AuthenticatedRequest extends Request {
+  user: { _id: string };
+}
+
+// logout user
+export const logoutUser = CatchAsyncError(
+  async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    try {
+      res.cookie("access_token", "", { maxAge: 1 });
+      res.cookie("refresh_token", "", { maxAge: 1 });
+      const userId = req.user?._id || "";
+      redis.del(userId);
+      res.status(200).json({
+        success: true,
+        message: "Logged out successfully",
+      });
+    } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
     }
   }
