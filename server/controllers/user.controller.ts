3816ee6e684377b26db4463361823de4cd2965c5
khaibtros <trankhai15052005@gmail.com>
2025-02-06 08:27:14 +0700

    login user

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -128,3 +128,39 @@ export const activateUser = CatchAsyncError(
     }
   }
 );
+
+// Login user
+interface ILoginRequest {
+  email: string;
+  password: string;
+}
+
+export const loginUser = CatchAsyncError(
+  
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log('loginuser')
+    try {
+      
+      const { email, password } = req.body as ILoginRequest;
+
+      if (!email || !password) {
+        return next(new ErrorHandler("Please enter email and password", 400));
+      }
+
+      const user = await userModel.findOne({ email }).select("+password");
+
+      if (!user) {
+        return next(new ErrorHandler("Invalid email or password", 400));
+      }
+
+      const isPasswordMatch = await user.comparePassword(password);
+      if (!isPasswordMatch) {
+        return next(new ErrorHandler("Invalid email or password", 400));
+      }
+      sendToken(user, 200, res);
+    } catch (error: any) {
+
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
