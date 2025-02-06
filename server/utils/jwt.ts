eb35014b0f925ac9ca2cb6a2e017fa1fa31d6bc7
khaibtros <trankhai15052005@gmail.com>
2025-02-06 21:38:05 +0700

    fix: user._id as string

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -43,7 +43,7 @@ export const sendToken = (user: IUser, statusCode: number, res: Response) => {
   const refreshToken = user.SignRefreshToken();
 
   // upload session to redis
-  redis.set(user._id.toString(), JSON.stringify(user) as any);
+  redis.set(user._id as string, JSON.stringify(user) as any);
 
   res.cookie("access_token", accessToken, accessTokenOptions);
   res.cookie("refresh_token", refreshToken, refreshTokenOptions);
