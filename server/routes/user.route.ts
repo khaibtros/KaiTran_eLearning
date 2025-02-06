d2cb9289c8d92f703f105b4949b20231a6f797da
khaibtros <trankhai15052005@gmail.com>
2025-02-06 08:41:43 +0700

    route loginUser

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import {
   registrationUser,
-  activateUser
+  activateUser,
+  loginUser
 } from "../controllers/user.controller";
 
 const userRouter = express.Router();
 
 userRouter.post("/registration", registrationUser);
 userRouter.post("/activate-user", activateUser);
+userRouter.post("/login", loginUser);
 
 export default userRouter;
