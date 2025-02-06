c7acb7b0d0e5e6ef379c7a89f04bd3bfdc9b539f
khaibtros <trankhai15052005@gmail.com>
2025-02-06 21:24:31 +0700

    authorize role

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -44,3 +44,18 @@ export const isAutheticated = CatchAsyncError(
     }
   }
 );
+
+// validate user role
+export const authorizeRoles = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user?.role || "")) {
+      return next(
+        new ErrorHandler(
+          `Role: ${req.user?.role} is not allowed to access this resource`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
