4f7462b84debb31dd08131aa982dd6c6fe872aca
khaibtros <trankhai15052005@gmail.com>
2025-02-06 20:32:09 +0700

    fix: remove _id form User Model, check if user is authenticated middleware

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -2,12 +2,10 @@ require("dotenv").config();
 import mongoose, { Document, Model, Schema } from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { Types } from "mongoose";
 
 const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 export interface IUser extends Document {
-  _id: Types.ObjectId;
   name: string;
   email: string;
   password: string;
