fb6e83ad62c2d3cab534ed9704cc102076eb8c28
khaibtros <trankhai15052005@gmail.com>
2025-02-12 09:07:22 +0700

    update user info controller

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -277,3 +277,35 @@ export const socialAuth = CatchAsyncError(
     }
   }
 );
+
+// update user info
+interface IUpdateUserInfo {
+  name?: string;
+  email?: string;
+}
+
+export const updateUserInfo = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name } = req.body as IUpdateUserInfo;
+
+      const userId = req.user?._id;
+      const user = await userModel.findById(userId);
+
+      if (name && user) {
+        user.name = name;
+      }
+
+      await user?.save();
+
+      await redis.set(userId as string, JSON.stringify(user));
+
+      res.status(201).json({
+        success: true,
+        user,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
