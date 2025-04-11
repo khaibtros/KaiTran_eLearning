3f59e910f1dbbc4d1858007b0812cd65afa944c5
khaibtros <trankhai15052005@gmail.com>
2025-04-11 23:25:08 +0700

    update noti status (controller and routing)

--- server/routes/notification.route.ts
+++ server/routes/notification.route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
-import { getNotifications } from "../controllers/notification.controller";
+import { getNotifications, updateNotification } from "../controllers/notification.controller";
 const notificationRouter = express.Router();
 
 notificationRouter.get(
@@ -9,5 +9,6 @@ notificationRouter.get(
   authorizeRoles("admin"),
   getNotifications
 );
+notificationRouter.put("/update-notification/:id", isAutheticated, authorizeRoles("admin"), updateNotification);
 
 export default notificationRouter;
