1980299bb3105b3bf8e7818227112ed0106c4329
khaibtros <trankhai15052005@gmail.com>
2025-04-12 22:29:30 +0700

    get all orders

--- server/controllers/order.controller.ts
+++ server/controllers/order.controller.ts
@@ -109,3 +109,14 @@ export const createOrder = CatchAsyncError(
     }
   }
 );
+
+// get All orders --- only for admin
+export const getAllOrders = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      getAllOrdersService(res);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
