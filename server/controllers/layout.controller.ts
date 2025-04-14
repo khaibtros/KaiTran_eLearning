15ab492b9dd615faaf5fbf7f2bfec863292bd9bf
khaibtros <trankhai15052005@gmail.com>
2025-04-15 00:00:20 +0700

    Get layout by type controlelr

--- server/controllers/layout.controller.ts
+++ server/controllers/layout.controller.ts
@@ -144,3 +144,19 @@ export const editLayout = CatchAsyncError(
     }
   }
 );
+
+// get layout by type
+export const getLayoutByType = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { type } = req.params;
+      const layout = await LayoutModel.findOne({ type });
+      res.status(201).json({
+        success: true,
+        layout,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
