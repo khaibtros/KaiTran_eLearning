1b2046f9ab8682eea6e783ea001b279aaf8ce4c1
khaibtros <trankhai15052005@gmail.com>
2025-04-27 01:10:25 +0700

    Hero, Auth

---
+++ client/redux/features/store.ts
@@ -0,0 +1,25 @@
+"use client";
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./api/apiSlice";
+import authSlice from "./auth/authSlice";
+
+export const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authSlice,
+  },
+  devTools: false,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+// call the load user function on every page load
+const initializeApp = async () => {
+  await store.dispatch(
+    apiSlice.endpoints.loadUser.initiate({}, { forceRefetch: true })
+  );
+};
+
+initializeApp();
